b11df8be7f1fdbc9dac45e2a133410307b851044
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-11-27 15:49:48 +0530

    removed: CountVectorizer import

--- Classification/NaiveBayes/gaussian.py
+++ Classification/NaiveBayes/gaussian.py
@@ -1,7 +1,6 @@
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import matplotlib.pyplot as plt
